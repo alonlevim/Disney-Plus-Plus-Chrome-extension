aa1f34d289429589469a2d75f53053d00b5ffd70
alonlevim <alonlevim@gmail.com>
2022-11-19 12:45:05 +0200

    Environment setup

---
+++ src/contentScripts/index.ts
@@ -0,0 +1,5 @@
+const init = (first = true): void => {
+    //
+};
+
+window.onload = () => init(true);
---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const CopyPlugin = require("copy-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
+const path = require("path");
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = (env, argv) => ({
+   mode: 'production',
+
+   devtool: 'source-map',
+
+   entry: {
+      service_worker: './src/background/index.ts',
+      content_scripts: './src/contentScripts/index.ts',
+   },
+
+   output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: '[name].js',
+      clean: true,
+   },
+
+   resolve: {
+      extensions: ['.ts', '.js']
+   },
+
+   module: {
+      rules: [{
+         test: /\.tsx?$/,
+         use: 'ts-loader',
+         exclude: /node_modules/,
+      }],
+   },
+
+   plugins: [
+      new ESLintPlugin({
+         overrideConfigFile: path.resolve(__dirname, '.eslintrc.json')
+      }),
+      new CopyPlugin({
+         patterns: [{
+            from: ".",
+            to: ".",
+            context: "public"
+         }]
+      }),
+      new Dotenv({
+         path: `./src/${argv.mode}.env`
+      }),
+   ],
+
+   watch: argv.mode === "development",
+
+   watchOptions: {
+      poll: true,
+      ignored: /node_modules/
+
+   }
+});
