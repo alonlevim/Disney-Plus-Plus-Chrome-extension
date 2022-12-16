b7a4a9fe38068026af8ed15a25b3c3ed7719f652
alonlevim <alonlevim@gmail.com>
2022-12-16 19:12:45 +0200

    Fix release

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 version="$(cat public/manifest.json | grep \"version\" | sed 's/\"version\": \"//' | sed 's/\",//' | sed -e 's/^[[:space:]]*//')"
 
-npm run build
+npm run build:all
 
 # # create zip
 zip -r -D "releases/$version.zip" dist/* --exclude \*.map\*
